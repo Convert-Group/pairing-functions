7a13d6b1bf467e313620d54a6acc296c4bc85f70
kkoutsioubi <kkoutsioubi@convertgroup.com>
2020-06-03 11:57:43 +0300

    [feat]: cantor pairing function

---
+++ pairing_functions/cantor.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from collections import deque
+from math import pow, floor, sqrt
+
+
+def pair(*numbers: int) -> int:
+    """
+    Maps a pair of non-negative integers to a uniquely associated single non-negative integer.
+    Pairing also generalizes for `n` non-negative integers, by recursively mapping the first pair.
+    For example, to map the following tuple:
+    (n_1, n_2, n_3)
+    the n_1, n_2 pair is mapped accordingly to a number n_p,
+    and then the n_p, n3 pair is mapped to produce the final association.
+    """
+    if len(numbers) < 2:
+        raise ValueError('Cantor pairing function needs at least 2 numbers as input')
+
+    elif any((n < 0) or (not isinstance(n, int)) for n in numbers):
+        raise ValueError('Cantor pairing function maps only non-negative integers')
+
+    numbers = deque(numbers)
+
+    # fetch the first two numbers
+    n1 = numbers.popleft()
+    n2 = numbers.popleft()
+
+    mapping = (n1 + n2)*(n1 + n2 + 1) / 2 + n2
+
+    mapping = int(mapping)
+
+    if not numbers:
+        # recursion concludes
+        return mapping
+    else:
+        numbers.appendleft(mapping)
+        return pair(*numbers)
+
+
+def unpair(number: int, n: int = 2) -> tuple:
+    """
+    The inverse function outputs the pair associated with a non-negative integer.
+    Unpairing also generalizes by recursively unpairing a non-negative integer to `n` non-negative integers.
+    For example, to associate a `number` with three non-negative
+    integers n_1, n_2, n_3, such that:
+
+    pairing((n_1, n_2, n_3)) = `number`
+
+    the `number` will first be unpaired to n_p, n_3, then the n_p will be unpaired to n_1, n_2,
+    producing the desired n_1, n_2 and n_3.
+    """
+    if (number < 0) or (not isinstance(number, int)):
+        raise ValueError('Cantor unpairing function requires a non-negative integer')
+
+    w = floor((sqrt(8 * number + 1) - 1) / 2)
+    t = (pow(w, 2) + w) / 2
+
+    n2 = int(number - t)
+    n1 = int(w - n2)
+
+    if n > 2:
+        return unpair(n1, n - 1) + (n2,)
+    else:
+        # recursion concludes
+        return n1, n2
---
+++ tests/test_cantor.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from pairing_functions.cantor import pair, unpair
+
+
+class TestCantorPairing(object):
+
+    def test_list_pair(self):
+
+        assert pair(0, 0) == 0
+        assert pair(0, 1) == 2
+        assert pair(1, 0) == 1
+        assert pair(3, 4) == 32
+        assert pair(92, 23) == 6693
+        assert pair(1, 2, 3) == 69
+        assert pair(3, 4, 5) == 708
+        assert pair(1, 2, 3, 4) == 2705
+
+    def test_tuple_pair(self):
+
+        assert pair(2, 2) == 12
+        assert pair(3, 4) == 32
+        assert pair(1, 2, 3, 4, 5) == 3673410
+
+    def test_pair_exceptions(self):
+
+        with pytest.raises(ValueError):
+            assert pair(1)
+
+        with pytest.raises(ValueError):
+            assert pair(1, -2)
+
+        with pytest.raises(ValueError):
+            assert pair(1,)
+
+        with pytest.raises(ValueError):
+            assert pair(1, -2)
+
+
+class TestCantorUnpair(object):
+
+    def test_unpair(self):
+
+        assert unpair(0) == (0, 0)
+        assert unpair(1) == (1, 0)
+        assert unpair(2) == (0, 1)
+        assert unpair(32) == (3, 4)
+        assert unpair(6693) == (92, 23)
+
+        assert unpair(69) == (8, 3)
+        assert unpair(69, n=3) == (1, 2, 3)
+
+        assert unpair(708) == (32, 5)
+        assert unpair(708, n=3) == (3, 4, 5)
+
+        assert unpair(2705) == (69, 4)
+        assert unpair(2705, n=3) == (8, 3, 4)
+        assert unpair(2705, n=4) == (1, 2, 3, 4)
+
+    def test_unpair_exceptions(self):
+
+        with pytest.raises(ValueError):
+            assert unpair(0.5)
+
+        with pytest.raises(ValueError):
+            assert unpair(-1)
+
+
+class TestCantor(object):
+
+    def test_inverse_property(self):
+
+        n1, n2 = unpair(pair(1, 2))
+        assert n1 == 1 and n2 == 2
+
+        n1, n2 = unpair(33)
+        assert pair(n1, n2) == 33
